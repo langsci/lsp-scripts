be567618a9f9dcfdc96c8c59c44e9870fdf8fad8
Sebastian Nordhoff <sebastian.nordhoff@langsci-press.org>
2015-07-13 15:02:26 +0200

    add documentation

--- webstats/extractaw.py
+++ webstats/extractaw.py
@@ -1,6 +1,5 @@
 import glob
-import re
-#import matplotlib
+import re 
 import os
 import BeautifulSoup
 import sys
@@ -9,8 +8,11 @@ import matplotlib.pyplot as plt
   
 class Catalog():
   def __init__(self, books='books.tsv'):
+    #read ID and title of books from file
     self.books = dict([l.strip().split('\t') for l in open(books).readlines()]) 
+    #collect all directories with access information
     self.dirs = glob.glob('webreport_langsci-press.org_catalog_20[0-9][0-9]_[01][0-9]')
+    #extract access data from all log files
     self.monthstats = dict([(d[-7:],Stats(os.path.join(d,'awstats.langsci-press.org.urldetail.html')).getBooks()) for d in self.dirs]) 
   
   def plotall(self): 
@@ -19,7 +21,9 @@ class Catalog():
 	self.plot(book, self.monthstats[month][book]) 
 	
   def plotaggregate(self):
-    d = {}
+    """ compute totals for books and print out """
+    
+    d = {}    
     for month in self.monthstats: 
       for book in self.monthstats[month]:
 	try:
@@ -30,6 +34,8 @@ class Catalog():
 	self.plot(book, d[book])
 	
   def plotcumulative(self):
+    """ compute totals for books per month and print out """
+    
     d = {}
     for month in sorted(self.monthstats):
       print ''
@@ -43,40 +49,71 @@ class Catalog():
 	self.plot(book, d[book]) 
 	
   def plot(self,book,hits):
+    """print to standard out"""
+    
     print hits, hits/20*'|', self.books[str(book)]
     
   def matplotcumulative(self):
+    """
+    produce cumulative graph
+    
+    Aggregate cumulative data for time sequence.
+    Plot this data with matplotlib.
+    """
+    
+    #sort the keys so we get them in temporal order
     labels = sorted(self.monthstats.keys())
     print labels
+    
+    #setup matplot 
     fig = plt.figure()
+    #use a wide picture
     fig.set_figwidth(12)
     ax = plt.subplot(111)
+    
+    #setup colors and shapes to select from
     colors = 'bgrcmyk'
     shapes = 'v^osp*+xD'
+    
+    #store data to plot here so we can sort before plotting
     plots = []
     for book in self.books:
       print book,':',
       tmp = 0 
-      x = [i for i in range(len(labels))]
+      #initialize axes
+      x = range(len(labels))
       y = [0 for i in range(len(labels))]
+      #update values for axes
       for i,month in enumerate(labels):	 
 	try:
 	  y[i] = tmp+self.monthstats[month][int(book)]
 	  tmp = y[i]
-	except KeyError:
+	except KeyError:#no downloads this month
 	  y[i] = tmp
       print y  
+      #colors and shapes for lines should be identical for 
+      #a book across several graphics, but different for 
+      #different books. Use a hash function to assign colors
+      #and shapes
       seed = hash(book)
-      c = colors[hash(book)%len(colors)]
-      s = shapes[hash(book)%len(shapes)]
+      c = colors[seed%len(colors)]
+      s = shapes[seed%len(shapes)]
+      #store plot data for future usage
       plots.append((x,y,c,s,self.books[book])) 
+    #sort plot data according to highest total downloads
+    #Then plot the plots
     for plot in sorted(plots, key=lambda k: k[1][-1],reverse=True): 
+      #plot line
       ax.plot(plot[0],plot[1],'%s-'%plot[2],linewidth=1.5)
+      #plot marks
       ax.plot(plot[0],plot[1],'%s%s'%(plot[2],plot[3]),label=plot[4])
+    #plot x-axis labels
     plt.xticks(x, [l[-5:].replace('-.','/') for l in labels])
+    #position legend box
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.66, box.height]) 
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    #save file
     plt.savefig('test.png') 
 	
 	  
@@ -85,18 +122,40 @@ class Catalog():
     
 class Stats():
   def __init__(self,f):
-    self.hits = dict([(tr.findAll('td')[0].text,int(tr.findAll('td')[1].text.replace(',',''))) for tr in BeautifulSoup.BeautifulSoup(open(f)).find('table',attrs={'class':'aws_data'}).findAll('tr')[1:]])
+    """
+    navigate the html file to find the relevant <td>s and
+    create a dictionary mapping urls to download figures
+    """
+    
+    self.hits = dict(
+		      [
+			(
+			  #locate key
+			  tr.findAll('td')[0].text,
+			  #remove thousands separator and convert value to int
+			  int(tr.findAll('td')[1].text.replace(',',''))
+			) for tr in BeautifulSoup.BeautifulSoup(open(f))\
+				  .find('table',attrs={'class':'aws_data'})\
+				  .findAll('tr')[1:]
+		      ]
+		  )
     
   def getBooks(self):
+    """
+    analyze the access data and aggregate stats for books across publication formats
+    """
+    
     d = {}
     for k in self.hits:
-      if 'view' in k:
+      if 'view' in k: #ignore /download/, which is used for files other than pdf
 	try:
+	  #extract ID
 	  i = int(re.search('view/([0-9]+)',k).groups()[0])
 	except AttributeError:
 	  print "no valid book key in %s" %k
 	  continue
 	try:
+	  #accumulate figures for the various publication formats
 	  d[i] += self.hits[k]
 	except KeyError:
 	  d[i] = self.hits[k]
@@ -104,9 +163,5 @@ class Stats():
     
 if __name__=='__main__':
   c = Catalog()
-  c.matplotcumulative()
-  #f = sys.argv[1]
-  #d = Stats(f).getBooks()
-  #for k in d:
-    #print k, d[k], d[k]/10*'|'
+  c.matplotcumulative() 
 	
